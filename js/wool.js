718833b3c1863c71bce42fe85c52ce40b5dff483
abaddonGIT <abaddongit@gmail.com>
2013-08-03 23:08:40 +0400

    2

--- js/wool.js
+++ js/wool.js
@@ -4,3 +4,17 @@
  * @version 1.0.0
  * ***************************************************/
 /*global window, $, jQuery, document */
+(function () {
+    var Wool = {
+        render: undefined,
+        repDelimeters: /\{\{=([\s\S]+?)\}\}/g,
+        ventDelimeters: /\{\{([\s\S]+?)\}\}/g
+    };
+    /*
+    * @param tpl {String} код шаблона
+    * @param data {Object} данные замены
+    */
+    Wool.render = function (tpl, data) {
+        var str = tpl;
+    }
+});
